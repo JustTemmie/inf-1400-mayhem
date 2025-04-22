c36c1ff2f091d06cbae958c5539ee7382a5c7557
JustTemmie <47639983+JustTemmie@users.noreply.github.com>
2025-04-23 00:15:41 +0200

    uhhhh, i had to commit to pull

---
+++ mayhem/entities2D/menus/MainMenu.py
@@ -0,0 +1,9 @@
+"""
+Very simple entity that just displays the player's score in the top left of the screen.
+Authors: JustTemmie (i'll replace names at handin)
+"""
+
+from mayhem.entities2D.menus.Menu import Menu
+
+class MainMenu(Menu):
+    pass
