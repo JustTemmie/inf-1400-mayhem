670dcd7c98e2e2480ec01094a7a0a5090c4edab1
BAaboe <benjamin.mjaatvedt@gmail.com>
2025-04-25 10:11:00 +0200

    gravitational constat should be correct

--- mayhem/entities/obstacles/Planet.py
+++ mayhem/entities/obstacles/Planet.py
@@ -14,7 +14,7 @@ import pyglet
 
 class Planet(Obstacle):
     def user_init(self):
-        self.mass = 80_000_000  # kg
+        self.mass = 6*10**13  # kg
         self.area = 1  # m^2, affects the objects interaction with air
         self.ignore_friction: bool = True
         self.rotation_velocity = Vec3(0.02, 0, 0)
